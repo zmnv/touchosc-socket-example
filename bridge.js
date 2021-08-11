d8d713f07ae858d913b499b21c90277090b202e4
Valeriy <sorption.music@gmail.com>
2021-08-12 01:52:40 +0300

    TouchOSC -> Socket

---
+++ bridge.js
@@ -0,0 +1,51 @@
+const osc = require('osc');
+
+const WebSocket = require('ws');
+const server = new WebSocket.Server({
+    host: '127.0.0.1',
+    port: 8080
+});
+
+let socketInstance;
+
+// server.on('error', function (error) {
+//     console.log('WebSocket Error:', error);
+// })
+
+server.on('connection', function (socket) {
+    console.log('Somebody Connected!');
+    socketInstance = socket;
+
+    socket.on('close', function () {
+        socketInstance = undefined;
+        console.log('WebSocket closed!');
+    });
+});
+
+// ---
+
+// Create an osc.js UDP Port listening on port 57121.
+var udpPort = new osc.UDPPort({
+    localAddress: "0.0.0.0",
+    localPort: 57121,
+    metadata: true
+});
+
+// Listen for incoming OSC messages.
+udpPort.on("message", function (oscMsg, timeTag, info) {
+    // console.log("An OSC message just arrived!", oscMsg);
+    // console.log("Remote info is: ", info);
+
+    if (!socketInstance) {
+        console.log('Error! No connection with sockets.');
+        return;
+    }
+
+    const message = JSON.stringify(oscMsg);
+
+    socketInstance.send(message);
+
+});
+
+// Open the socket.
+udpPort.open();
---
+++ socket-client.js
@@ -0,0 +1,29 @@
+var socket = new WebSocket("ws://127.0.0.1:8080");
+
+socket.onopen = function () {
+    console.log("Соединение установлено.");
+};
+
+socket.onclose = function (event) {
+    if (event.wasClean) {
+        console.log('Соединение закрыто чисто');
+    } else {
+        console.log('Обрыв соединения'); // например, "убит" процесс сервера
+    }
+    console.log('Код: ' + event.code + ' причина: ' + event.reason);
+};
+
+socket.onmessage = function (event) {
+    try {
+        const data = JSON.stringify(event.data);
+        console.log("Получены данные " + );
+    } catch (e) {
+        console.error('Ошибка в расшифровке данных, это не объект? Подробнее:', e);
+    }
+
+    // window.GUI.object.DEPOSITE = Number(event.data)
+};
+
+socket.onerror = function (error) {
+    console.log("Ошибка " + error.message);
+};
